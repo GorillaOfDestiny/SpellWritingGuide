54ae769264cf4e273fd513f3a8fc2d1fbf96a8db
GorillaOfDestiny <gorillaofdestinytiktok@gmail.com>
2024-10-21 18:44:09 +0100

    Fixed bug with out of date attributes

--- writer.py
+++ writer.py
@@ -55,7 +55,7 @@ def generate_binary_strings(bit_count):
     return binary_strings
 
 #-------Functions for drawing runes
-def decode_shape(in_array,k=1,point_color = 'k',color = 'k',
+def decode_shape(in_array,k=1,point_color = 'k',on_color = 'darkred',off_color = "grey",
                  label = None,base_fn = bases.polygon,base_kwargs = [],
                  shape_fn = line_shapes.straight,shape_kwargs = [],
                  plot_base = False):
@@ -72,9 +72,10 @@ def decode_shape(in_array,k=1,point_color = 'k',color = 'k',
         Q = [x[(i+k)%n],y[(i+k)%n]]
         X,Y = shape_fn(P,Q,*shape_kwargs)
         if elem == 0:
-            plt.plot(X,Y,color = color,ls = ":",linewidth=0.5)
+            plt.plot(X,Y,color = off_color,ls = "--",linewidth=0.25)
         elif elem == 1:
-            plt.plot(X,Y,color = color,ls = "-",label = label if i == np.where(in_array == 1)[0][0] else None)
+            plt.plot(X,Y,color = on_color,ls = "-",label = label if i == np.where(in_array == 1)[0][0] else None,
+                     linewidth = 2)
         else:
             print(f'elem {elem} at index {i} is not valid, input being skipped')
     
@@ -86,8 +87,10 @@ def draw_multiple_inputs(in_array,
                          legend_loc = "upper left"):
     
     #draws multiple inputs on a single base
-    if colors == []:
+    if isinstance(colors,list) and len(colors) == 0:
         colors = [point_color]*in_array.shape[0]
+    elif isinstance(colors,str):
+        colors = [colors]*in_array.shape[0]
     n = in_array.shape[1]
     x,y = base_fn(n,*base_kwargs)
     plt.scatter(x[1:],y[1:],s = 70,facecolors = 'none', edgecolors = point_color)
@@ -97,22 +100,24 @@ def draw_multiple_inputs(in_array,
         labels = [None]*in_array.shape[0]
 
     for i,k in enumerate(range(in_array.shape[0])):
-        
+
         decode_shape(in_array[i],k = k+1,base_fn = base_fn,base_kwargs = base_kwargs,
-                     shape_fn = shape_fn,shape_kwargs = shape_kwargs,label = labels[i],color = colors[i])
+                     shape_fn = shape_fn,shape_kwargs = shape_kwargs,label = labels[i],on_color = colors[i])
+
     if labels[0] != None and legend == True:
-        plt.legend(loc = legend_loc,fontsize = 4)
+        plt.legend(loc = legend_loc,fontsize = 10)
     plt.axis('off')
     plt.axis('scaled')
 def load_attribute(fname):
     with open(fname,"r") as f:
         data = f.readlines()
         f.close()
-    data = [d.replace("\n","") for d in data]
+    data = [d.replace("\n","").lower() for d in data]
     return(data)
 
 
-def draw_spell(level,rang,area,dtype,school,title = None,savename = "output.png",legend = False,
+def draw_spell(level,rang,area,dtype,school,title = None,
+               savename = "output.png",legend = False,
                 base_fn = bases.polygon,base_kwargs = [],
                 shape_fn = line_shapes.straight,shape_kwargs = [],
                 colors = [],legend_loc = "upper left",breakdown = False):
@@ -152,14 +157,145 @@ def draw_spell(level,rang,area,dtype,school,title = None,savename = "output.png"
                          base_fn = base_fn,base_kwargs = base_kwargs,
                          shape_fn = shape_fn,shape_kwargs = shape_kwargs,
                          colors = colors,legend_loc = legend_loc)
+    
+    plt.title(title,fontsize = "80")
+    
+    if savename is not None:
+        plt.savefig(savename,transparent = False, bbox_inches='tight')
+        plt.clf()
+    else:
+        plt.show()
+
+def draw_spell_2(level,rang,area,dtype,school,duration,concentration,ritual,title = None,
+               savename = "output.png",legend = False,
+                base_fn = bases.polygon,base_kwargs = [],
+                shape_fn = line_shapes.straight,shape_kwargs = [],
+                colors = [],legend_loc = "upper left",breakdown = False,
+                base_dir = ""):
+    
+    #draws a spell given certain values by comparing it to input txt
+    ranges = load_attribute(base_dir +"Attributes/range.txt")
+    levels = load_attribute(base_dir +"Attributes/levels.txt")
+    area_types = load_attribute(base_dir +"Attributes/area_types.txt")
+    dtypes = load_attribute(base_dir +"Attributes/damage_types.txt")
+    schools = load_attribute(base_dir +"Attributes/school.txt")
+    durations = load_attribute(base_dir +"Attributes/duration.txt")
+    i_range = ranges.index(rang)
+    i_levels = levels.index(str(level))
+    i_area = area_types.index(area)
+    i_dtype = dtypes.index(dtype)
+    i_school = schools.index(school)
+    i_duration = durations.index(duration)
+    attributes = [i_levels,i_school,i_dtype,i_area,i_range,i_duration]
+    labels = [f"level: {level}",
+              f"school: {school}",
+              f"damage type: {dtype}",
+              f"range: {rang}",
+              f"area_type: {area}",
+              f'duration: {duration}']
+    N = 2*len(attributes)+1
+    
+    if len(colors) == 0 and breakdown == True:
+        colors = [cmap(i/len(attributes)) for i in range(len(attributes))]
+    if not os.path.isdir(base_dir +"Uniques/"):
+        os.makedirs(base_dir +"Uniques/")
+    if os.path.isfile(base_dir +f'Uniques/{N}.npy'):
+        non_repeating = np.load(base_dir +f'Uniques/{N}.npy')
+    else:
+        non_repeating = generate_unique_combinations(N)
+        non_repeating = np.array(non_repeating)
+        np.save(base_dir +f"Uniques/{N}.npy",non_repeating)
+    input_array = np.array([non_repeating[i] for i in attributes])#note +1 s.t. 0th option is always open for empty input
+    
+    draw_multiple_inputs(input_array,labels = labels,legend = legend,
+                         base_fn = base_fn,base_kwargs = base_kwargs,
+                         shape_fn = shape_fn,shape_kwargs = shape_kwargs,
+                         colors = colors,legend_loc = legend_loc)
+    
+    if concentration:
+        plt.plot(0,0,"",markersize = 10,marker = ".",color = colors)
+    if ritual:
+        
+        plt.plot(0,0,"",markersize = 10,marker = ".",color= colors)
+        plt.plot(0,0,"",markersize = 20,marker = "o",color=colors,mfc='none',linewidth = 10)
+    
     plt.title(title)
     if savename is not None:
-        plt.savefig(savename,dpi = 250,transparent = True, bbox_inches='tight')
+        plt.savefig(savename,transparent = True, bbox_inches='tight')
+        plt.clf()
     else:
         plt.show()
 
 #to run if the file is called
+def draw_attribute(level = None,rang = None, area = None,
+                   savename = "output.png",legend = False,
+                    dtype = None, school = None,duration = None,
+                    base_fn = bases.polygon,base_kwargs = [],
+                shape_fn = line_shapes.straight,shape_kwargs = [],
+                colors = [],legend_loc = "upper left",breakdown = False,
+                title = None):
+    ranges = load_attribute("Attributes/range.txt")
+    levels = load_attribute("Attributes/levels.txt")
+    area_types = load_attribute("Attributes/area_types.txt")
+    dtypes = load_attribute("Attributes/damage_types.txt")
+    schools = load_attribute("Attributes/school.txt")
+    durations = load_attribute("Attributes/duration.txt")
 
+    i_range,i_levels,i_school,i_dtype,i_area,i_duration = 0,0,0,0,0,0
+    if rang is not None:
+        i_range = ranges.index(rang)
+
+    elif level is not None:
+        i_levels = levels.index(str(level))
+
+    elif area is not None:
+        i_area = area_types.index(area)
+
+    elif dtype is not None:
+        i_dtype = dtypes.index(dtype)
+
+    elif school is not None:
+        i_school = schools.index(school)
+
+    elif duration is not None:
+        i_duration = durations.index(duration)
+    attributes = [i_levels,i_school,i_dtype,i_area,i_range,i_duration]
+    labels = [f"level: {level}",
+            f"school: {school}",
+            f"damage type: {dtype}",
+            f"range: {rang}",
+            f"area_type: {area}",
+            f"duration: {duration}"]
+    
+    N = 2*len(attributes)+1
+    
+    if isinstance(colors,list) and len(colors) == 0 and breakdown == True:
+        colors = [cmap(i/len(attributes)) for i in range(len(attributes))]
+    if not os.path.isdir("Uniques/"):
+        os.makedirs("Uniques/")
+    if os.path.isfile(f'Uniques/{N}.npy'):
+        non_repeating = np.load(f'Uniques/{N}.npy')
+    else:
+        non_repeating = generate_unique_combinations(N)
+        non_repeating = np.array(non_repeating)
+        np.save(f"Uniques/{N}.npy",non_repeating)
+    
+    input_array = []
+    for j,i in enumerate(attributes):
+        input_array.append(non_repeating[i])
+    input_array = np.array(input_array)#note +1 s.t. 0th option is always open for empty input
+    #print(input_array)
+    draw_multiple_inputs(input_array,labels = labels,legend = legend,
+                         base_fn = base_fn,base_kwargs = base_kwargs,
+                         shape_fn = shape_fn,shape_kwargs = shape_kwargs,
+                         colors = colors,legend_loc = legend_loc)
+    plt.title(title,fontsize = 30)
+    if savename is not None:
+        plt.savefig(savename,dpi = 250,transparent = True, bbox_inches='tight')
+        plt.clf()
+    else:
+        plt.show()
+    
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
@@ -220,12 +356,12 @@ if __name__ == "__main__":
             level = args.level
         
         if not args.range:
-            rang = "point (150 feet)"
+            rang = "150 feet"
         else:
             rang = args.range
         
         if not args.area:
-            area = "sphere"
+            area = "sphere (30)"
         else:
             area = args.area
         
